47ea56a77b09aa72f0a0b9dc53354aea7b3cd30b
LEEjunswer <jun_5949@naver.com>
2024-04-26 22:52:16 +0900

    Main Project 4/26
    Main Project 4/26

---
+++ src/main/java/com/HotUdon/HotUdon/HotUdonApplication.java
@@ -0,0 +1,13 @@
+package com.HotUdon.HotUdon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotUdonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotUdonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HotUdon/HotUdon/HotUdonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotUdon.HotUdon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotUdonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
